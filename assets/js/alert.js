75f52843530719d5b2e3d06a6dac37bab6b3e005
Krainel_SF <mazah124@gmail.com>
2024-11-22 09:38:25 +0300

    praktika7

---
+++ assets/js/alert.js
@@ -0,0 +1,12 @@
+'use strict'
+
+btn.addEventListener('mousedown', function(){
+    btn.style.display = 'none';
+})
+
+btn2.addEventListener('mousedown', function(){
+    text.style.display = 'none';
+})
+btn2.addEventListener('mouseup', function(){
+    text.style.display = 'block';
+})
